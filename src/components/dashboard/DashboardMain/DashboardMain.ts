37379d1bac59d5918d52ea1c21f4afe7c8cc46a0
GlennD12 <glenndalemagbanua@gmail.com>
2025-03-13 16:49:48 +0800

    json fix

--- src/components/dashboard/DashboardMain/DashboardMain.ts
+++ src/components/dashboard/DashboardMain/DashboardMain.ts
@@ -1,13 +1,25 @@
 import LinkHeaderMain from "@/components/shared/link_header/LinkHeaderMain/LinkHeaderMain.vue";
-export default {
+import { defineComponent } from "vue";
+import userDB from "@/assets/json/db.json";
+
+export default defineComponent({
   name: "DashboardMain",
   components: {
     LinkHeaderMain
   },
-  data(){
-    return{
+  data() {
+    return {
       pageName: "dashboard",
+      userData: userDB.users,
     }
+  },
+  mounted() {
+    console.log(this.pageName);
+    console.log(this.userData);
+    this.userData.forEach((item) => {
+      console.log("Email: ", item.email);
+      console.log("Name: ", item.name);
+    });
   }
-}
+})
 
